bde2bdb0e6123c88fc84d2b886ba4fad02684e9d
sdnair <sohannair25@gmail.com>
2024-10-23 18:06:41 -0600

    reverted changes

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -14,18 +14,22 @@ public class CustomListTest {
     private CustomList list;
 
     /**
-     * create a mocklist for my citylist * @return
+     * create a mocklist for my citylist
+     * @return
      */
-    public CustomList MockCityList() {
-        list = new CustomList(null, new ArrayList<>());
-        return list;
-    }
+    public CustomList MockCityList(){
+    list =  new CustomList(null,new ArrayList<>());
+    return list;
+}
+
 
     @Test
-    public void addCityTest() {
+    public void addCityTest(){
         list = MockCityList();
         int listSize = list.getCount();
-        list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.getCount(), listSize + 1);
+        list.addCity(new City("Estevan", "SM"));
+        assertEquals(list.getCount(),listSize + 1);
+
     }
+
 }
